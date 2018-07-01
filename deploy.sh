ee28f2bb5f0f4494c96b47f1bef4b6b9424f5d71
caoyongjie <m13036168227_1@163.com>
2018-07-01 16:24:29 +0800

    Signed-off-by: caoyongjie <m13036168227_1@163.com>

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 #关闭tomcat
-killTomcat()
-{
-   pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-   echo "tomcat Id LIST :$pid"
-   if [ "$pid" = "" ]
-   then
-     echo "----Tomcat-test 已经关闭----"
-   else
-      kill -9 $pid
-   fi
-}
-cd $PROJ_PATH/server_eureka_server
+#killTomcat()
+#{
+#   pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+#   echo "tomcat Id LIST :$pid"
+#   if [ "$pid" = "" ]
+#   then
+#     echo "----Tomcat-test 已经关闭----"
+#   else
+#      kill -9 $pid
+#   fi
+#}
+cd $PROJ_PATH/eureka-server
 mvn clean install
-#nohup java -jar server_eureka_server.jar
-killTomcat
-
-cp $PROJ_PATH/server_eureka_server/target/server_eureka_server.war $TOMCAT_APP_PATH/webapps/
-
-cd $TOMCAT_APP_PATH/
-
-sh bin/startup.sh
+nohup java -jar eureka-server.jar
+#killTomcat
+#
+#cp $PROJ_PATH/server_eureka_server/target/server_eureka_server.war $TOMCAT_APP_PATH/webapps/
+#
+#cd $TOMCAT_APP_PATH/
+#
+#sh bin/startup.sh
--- src/main/java/com/sie/Application.java
+++ src/main/java/com/sie/Application.java
@@ -2,18 +2,12 @@ package com.sie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer // 启用Eureka服务
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
