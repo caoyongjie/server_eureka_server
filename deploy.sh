7ee00803130c4b093985e1187e9d726dc00343e8
caoyongjie <m13036168227_1@163.com>
2018-07-01 16:04:38 +0800

    Signed-off-by: caoyongjie <m13036168227_1@163.com>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
-cd $PROJ_PATH
+killTomcat()
+    {
+        pid=`ps -ef | grep tomcat|grep java|awk '{print $2}'`
+        echo "tomcat Id LIST :$pid"
+        if [ "$pid" = "" ]
+        then
+            echo "----Tomcat-test 已经关闭----"
+        else
+            kill -9 $pid
+        fi
+    }
+
+cd $PROJ_PATH/server_eureka_server
 mvn clean install
-nohup java -jar server_eureka_server.jar
+#nohup java -jar server_eureka_server.jar
+killTomcat
+
+cp $PROJ_PATH/server_eureka_server/target/server_eureka_server.war $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/
+
+sh bin/startup.sh
--- src/main/java/com/sie/Application.java
+++ src/main/java/com/sie/Application.java
@@ -2,12 +2,18 @@ package com.sie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer // 启用Eureka服务
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
